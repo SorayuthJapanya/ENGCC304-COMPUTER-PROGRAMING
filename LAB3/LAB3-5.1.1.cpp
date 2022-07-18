e5894a95d4d9d5fd2f32ffabe5b789bafb754bee
Euro Sorayuth <108041976+SorayuthJapanya@users.noreply.github.com>
2022-07-18 21:54:52 +0700

    LAB3
    Codex
    printf scanf

---
+++ LAB3/LAB3-5.1.1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	
+	int   N ; // Number
+	char  T ; // Text
+	float D ; // Derminal
+	
+	printf( "Please enter value : " ) ;
+	scanf( " %d %f %c ", &N , &D , &T ) ;
+	printf( " %d %.2f %c ", N , D , T ) ;
+	return 0 ;
+	
+}// end function
